c3ae20aab7fdc0d469e8b889234102c05dc1c6cd
Dmitry <dmitrodonskoy@yandex.ru>
2026-01-21 15:43:03 +0300

    roject v2

--- src/components/ScheduleSessionSpeakers.js
+++ src/components/ScheduleSessionSpeakers.js
@@ -1,27 +1,5 @@
 import React, { useEffect, useMemo, useRef, useState } from 'react';
 
-const ArrowIcon = () => (
-  <svg
-    className="scheduleSpeakers__companyArrowIcon"
-    width="18"
-    height="18"
-    viewBox="0 0 24 24"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    aria-hidden="true"
-    focusable="false"
-  >
-    <path d="M7 12H17" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
-    <path
-      d="M13 8L17 12L13 16"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-  </svg>
-);
-
 const clamp = (value, min, max) => Math.min(max, Math.max(min, value));
 
 const renderMultiline = (text) =>
